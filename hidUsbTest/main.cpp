b26fa322ec4d4a237d2d49148057808982bb1772
NewLeafG <DOCAIatGoolegle@gmail.com>
2023-03-31 12:53:16 +0800

    完成qml和c++整合入门案例：定时器改变矩形颜色

--- hidUsbTest/main.cpp
+++ hidUsbTest/main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "colormaker.h"
 
 
 int main(int argc, char *argv[])
@@ -8,6 +9,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QGuiApplication app(argc, argv);
+    qmlRegisterType<ColorMaker>("an.qt.ColorMaker",1,0,"ColorMaker");
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
